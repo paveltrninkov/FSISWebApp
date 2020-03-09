beff1e0322f7fb5e21f1ea002b4097c532004bac
Pavel Trninkov <ptrninkov1@nait.ca>
2020-03-09 11:13:37 -0600

    Add project files.

---
+++ FSISSystem/DAL/FSISContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity; //inheritance of DbContext from EntityFramework
+using FSISSystem.ENTITIES;
+
+namespace FSISSystem.DAL
+{
+    internal class FSISContext : DbContext
+    {
+
+    }
+    
+}
---
+++ FSISSystem/ENTITIES/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSISSystem.ENTITIES
+{
+    class Player
+    {
+        public string FullName { get; set; }
+    }
+}
